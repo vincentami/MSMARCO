8cf07980a137bb374bdd3f15a9ea5756ccac9927
lilifengvincent <lilifengvincent@didichuxing.com>
2020-04-02 12:39:54 +0800

    adjust dt2

--- Ranking/Baselines/makeidf_vocab.py
+++ Ranking/Baselines/makeidf_vocab.py
@@ -7,6 +7,8 @@ MAX_DOC_TERMS = 400
 regex_drop_char = re.compile('[^a-z0-9\s]+')
 regex_multi_space = re.compile('\s+')
 
+IDF_DEFAULT=1
+
 if __name__ == "__main__":
     if len(sys.argv) != 5:
         print("Usage: makeidf.py <allTerm_file>  <allDev_file>  <allTrain_file>  <allIDF_file>")
@@ -18,19 +20,19 @@ if __name__ == "__main__":
             for line in reader:
                 cols = line.split('\t')
                 for t in regex_multi_space.sub(' ', regex_drop_char.sub(' ', cols[0].lower())).strip().split():
-                    df[t] = 0
+                    df[t] = IDF_DEFAULT
 
         with open(sys.argv[2], encoding = 'utf-8', mode='r') as reader:
             for line in reader:
                 cols = line.split('\t')
                 for t in regex_multi_space.sub(' ', regex_drop_char.sub(' ', cols[4].lower() + ' ' + cols[5].lower())).strip().split():
-                    df[t] = 0
+                    df[t] = IDF_DEFAULT
 
         with open(sys.argv[3], encoding = 'utf-8', mode='r') as reader:
             for line in reader:
                 cols = line.split('\t')
                 for t in regex_multi_space.sub(' ', regex_drop_char.sub(' ', cols[2].lower() )).strip().split():
-                    df[t] = 0
+                    df[t] = IDF_DEFAULT
 
         with open(sys.argv[4], encoding = 'utf-8', mode='r') as reader:
             for line in reader:
