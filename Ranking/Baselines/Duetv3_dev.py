4c6babdb3cf2fa3ad543b3093d9699bd467c7016
lilifengvincent <lilifengvincent@didichuxing.com>
2020-04-30 23:42:57 +0800

    adjust pre

--- Ranking/Baselines/Duetv3_dev.py
+++ Ranking/Baselines/Duetv3_dev.py
@@ -382,7 +382,7 @@ def goRun(device, reader_train, reader_dev, reader_eval, ts, name):
                                            for i in range(reader_train.num_docs)]
                                           ),1)
 
-                print_message("out type:{}".format(out))
+                print_message("out type:{}".format(out.shape))
 
                 loss = criterion(out, torch.from_numpy(features['labels']).to(device))
                 optimizer.zero_grad()
