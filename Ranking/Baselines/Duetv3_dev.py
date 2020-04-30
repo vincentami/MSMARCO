88ae4a933695336fdc014b86ceba80a57b0fc169
lilifengvincent <lilifengvincent@didichuxing.com>
2020-04-30 19:11:41 +0800

    debug

--- Ranking/Baselines/Duetv3_dev.py
+++ Ranking/Baselines/Duetv3_dev.py
@@ -302,7 +302,7 @@ class Duet(torch.nn.Module):
         y_out = self.duet_comb(
             (h_local + h_dist) if ARCH_TYPE == 2 else (h_dist if ARCH_TYPE == 1 else h_local))
 
-        pred = F.softmax(y_out, dim=0)
+        pred = F.softmax(y_out, dim=1)
 
         return pred[0]
         # for t in pred:
