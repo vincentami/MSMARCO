1dfb18e7699d515551ece842c3603841c502bd55
lilifengvincent <lilifengvincent@didichuxing.com>
2020-05-07 11:35:25 +0800

    adjust ds

--- Ranking/Baselines/Duetv3_dev.py
+++ Ranking/Baselines/Duetv3_dev.py
@@ -460,7 +460,9 @@ def goRun(device, reader_train, reader_dev, reader_eval, ts, name):
                 q = features['meta'][i][0]
                 d = features['meta'][i][1]
 
-                print_message("dev  meta_cnt:{} q:{}  d:{}  score:{}".format(i, q, d, score[i]))
+                res_score = score[i] if (predicted[i] == 1) else (1 - score[i])
+
+                print_message("dev  meta_cnt:{} q:{}  d:{}  score:{}".format(i, q, d, res_score))
                 #
                 # if q not in res_dev:
                 #     res_dev[q] = {}
