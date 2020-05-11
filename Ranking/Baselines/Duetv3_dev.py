f9a0fd191954ac13f979fc2ae79f91c62122fca2
lilifengvincent <lilifengvincent@didichuxing.com>
2020-05-11 18:03:23 +0800

    adjust train size

--- Ranking/Baselines/Duetv3_dev.py
+++ Ranking/Baselines/Duetv3_dev.py
@@ -500,7 +500,7 @@ def goEval(res_dev, df_dev):
 
     print_message("AUC Score (Train): {}".format(metrics.roc_auc_score(labelArr, preArr)))
 
-    y_pred = list(map(lambda x: 1 if x > 0.98 else 0, preArr))
+    y_pred = list(map(lambda x: 1 if float(x) > 0.98 else 0, preArr))
 
     print_message("Accuracy :{}".format(metrics.accuracy_score(labelArr, y_pred)))
 
